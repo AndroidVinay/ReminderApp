87ff26cd8db0498a4aa5658f0203e7aa9d2ad07e
VPPL-10132 <jambhalevinay@gmail.com>
2016-08-19 15:39:15 +0530

    second changes

--- app/src/main/java/com/vinay/reminderapp/FragmentIntervalList.java
+++ app/src/main/java/com/vinay/reminderapp/FragmentIntervalList.java
@@ -1,13 +1,18 @@
 package com.vinay.reminderapp;
 
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,44 +26,159 @@ public class FragmentIntervalList extends Fragment {
 	ListView intervalList;
 	SimpleAdapter simpleAdapter;
 	ArrayList<HashMap<String, String>> arrayIntervalList = new ArrayList<>();
+	int selectedPosition = 0;
+	AppCompatCheckBox rdb_interval;
 
 	public FragmentIntervalList() {
-		// Required empty public constructor
+
+
 	}
 
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	public View onCreateView(final LayoutInflater inflater, ViewGroup container,
 	                         Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
 		View view = inflater.inflate(R.layout.fragment_interval_list, container, false);
 		intervalList = (ListView) view.findViewById(R.id.list_interval);
 		HashMap<String, String> map = new HashMap<>();
 		map.put("data", "One-time event");
+		map.put("count", "");
 		arrayIntervalList.add(map);
 		HashMap<String, String> map1 = new HashMap<>();
 		map1.put("data", "Dialy");
+		map1.put("count", "");
 		arrayIntervalList.add(map1);
 		HashMap<String, String> map2 = new HashMap<>();
 		map2.put("data", "Every X Days");
+		map2.put("count", " 0 days");
 		arrayIntervalList.add(map2);
 		HashMap<String, String> map3 = new HashMap<>();
 		map3.put("data", "Weekly");
+		map3.put("count", "Mon,Tue");
 		arrayIntervalList.add(map3);
 		HashMap<String, String> map4 = new HashMap<>();
 		map4.put("data", "Monthly");
+		map4.put("count", "1,2,3");
 		arrayIntervalList.add(map4);
 		HashMap<String, String> map5 = new HashMap<>();
 		map5.put("data", "Every X Hours");
+		map5.put("count", "0 hours");
 		arrayIntervalList.add(map5);
 
 		simpleAdapter = new SimpleAdapter(getActivity(), arrayIntervalList, R.layout
-				.row_interval_list, new String[]{"data"}, new int[]{R.id.txt_interval_type});
+				.row_interval_list, new String[]{"data", "count"}, new int[]{R.id
+				.txt_interval_type, R.id.txt_count}) {
+
+			@Override
+			public View getView(final int position, View convertView, ViewGroup parent) {
+
+				View view = super.getView(position, convertView, parent);
+
+				rdb_interval = (AppCompatCheckBox) view.findViewById(R.id.rdb_interval);
+
+				rdb_interval.setTag(position);
+
+				if (position == selectedPosition) {
+					rdb_interval.setChecked(true);
+				} else {
+					rdb_interval.setChecked(false);
+				}
+
+				view.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						selectedPosition = position;
+
+						switch (position) {
+
+							case 0:
+								break;
+							case 1:
+								break;
+							case 2:
+
+
+								break;
+							case 3:
+								DialogFragment dialogFrag = ListDialogFragment.newInstance
+										("Select" +
+										" days", R.array.weekDays);
+								dialogFrag.setTargetFragment(FragmentIntervalList
+										.this, 1);
+								dialogFrag.show(getFragmentManager().beginTransaction(), "day");
+								break;
+							case 4:
+								DialogFragment dialogFrag1 = ListDialogFragment.newInstance
+										("Select month", R.array.months);
+								dialogFrag1.setTargetFragment(FragmentIntervalList
+										.this, 1);
+								dialogFrag1.show(getFragmentManager().beginTransaction(),
+										"month");
+								break;
+							case 5:
+								break;
+							default:
+								break;
+
+
+						}
+						notifyDataSetChanged();
+					}
+				});
+
+				return view;
+
+			}
+		};
 
 		intervalList.setAdapter(simpleAdapter);
-
 
 		return view;
 	}
 
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		super.onActivityResult(requestCode, resultCode, data);
+//		switch (requestCode){
+
+		if (resultCode == Activity.RESULT_OK) {
+			// After Ok code.
+			Bundle bundle = data.getExtras();
+			String result = bundle.getString("name");
+			Toast.makeText(getActivity(), "" + result, Toast.LENGTH_SHORT).show();
+		} else if (resultCode == Activity.RESULT_CANCELED) {
+			// After Cancel code.
+		}
+//		}
+	}
 }
+
+//class intervalModel {
+//
+//	String data;
+//	String count;
+//
+//	public intervalModel(String data, String count) {
+//		this.data = data;
+//		this.count = count;
+//	}
+//
+//	public String getData() {
+//		return data;
+//	}
+//
+//	public void setData(String data) {
+//		this.data = data;
+//	}
+//
+//	public String getCount() {
+//		return count;
+//	}
+//
+//	public void setCount(String count) {
+//		this.count = count;
+//	}
+//}
+
---
+++ app/src/main/java/com/vinay/reminderapp/ListDialogFragment.java
@@ -0,0 +1,100 @@
+package com.vinay.reminderapp;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.ArrayRes;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListDialogFragment extends DialogFragment {
+
+	ArrayList<Integer> mSelectedItems;
+	ArrayList<String> mSelectedData;
+	String[] arrayList;
+	String title;
+	@ArrayRes
+	int arrayData;
+	Context context;
+
+	public ListDialogFragment() {
+	}
+
+	public static ListDialogFragment newInstance(String title, @ArrayRes int data) {
+		// Required empty public constructor
+		ListDialogFragment dialogFragment = new ListDialogFragment();
+		dialogFragment.title = title;
+		dialogFragment.arrayData = data;
+
+		return dialogFragment;
+
+	}
+
+	@Override
+	public void onAttach(Context context) {
+		super.onAttach(context);
+		this.context = context;
+
+	}
+
+
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		mSelectedItems = new ArrayList();  // Where we track the selected items
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		// Set the dialog title
+		arrayList = context.getResources().getStringArray(arrayData);
+		mSelectedData = new ArrayList<>();
+		builder.setTitle(title)
+				// Specify the list array, the items to be selected by default (null for none),
+				// and the listener through which to receive callbacks when items are selected
+				.setMultiChoiceItems(arrayData, null,
+						new DialogInterface.OnMultiChoiceClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which,
+							                    boolean isChecked) {
+								if (isChecked) {
+									// If the user checked the item, add it to the selected items
+									mSelectedItems.add(which);
+									mSelectedData.add(arrayList[which]);
+								} else if (mSelectedItems.contains(which)) {
+									// Else, if the item is already in the array, remove it
+									mSelectedItems.remove(Integer.valueOf(which));
+								}
+							}
+						})
+				// Set the action buttons
+				.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+
+
+						getActivity().getIntent().putExtra("name", mSelectedData.toString());
+
+						getTargetFragment().onActivityResult(getTargetRequestCode(), Activity
+								.RESULT_OK, getActivity().getIntent());
+					}
+				})
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int id) {
+						getTargetFragment().onActivityResult(getTargetRequestCode(), Activity
+								.RESULT_CANCELED, getActivity().getIntent());
+					}
+				});
+
+		return builder.create();
+	}
+
+
+}
